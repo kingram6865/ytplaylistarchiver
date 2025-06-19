8dcc6837e842993efd316edeb392371c186b4c4c
Ken Ingram <ken.ingram@gmail.com>
2025-06-18 18:54:46 -0700

    Need to call API:videos to get video duration data.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import axios from 'axios';
-import { dbCheck, insertData, channelExists, addChannel, addVideo } from './db/dblib.js';
-import { executeMySQL } from './db/connect.js';
+import * as color from './utilities/consoleColorsES6.js'
+import { dbCheck, channelExists, addChannel, addVideo } from './db/dblib.js';
+
 
 const playlistUrl = process.env.PLAYLISTITEMS;
 const playlistInfo = process.env.PLAYLISTDATA;
 const channelAPIInfo = process.env.CHANNELDATA;
+const apiVideoInfo = process.env.VIDEODATA;
 const playlistItems = [];
 let playlistAttribs, channelInfo
 let data
@@ -15,12 +17,15 @@ async function retrievePlaylistitems(playlistid) {
     let response1 = await axios.get(`${playlistInfo}&id=${playlistid}`)
     let response = await axios.get(`${playlistUrl}&playlistId=${playlistid}`);
     let response3 = await axios.get(`${channelAPIInfo}&id=${response1.data.items[0].snippet.channelId}`)
+    
 
     playlistAttribs = response1.data.items[0]
     // console.log(response1.data.items[0].snippet)
     // console.log(response.data.pageInfo.totalResults);
     // console.log(response3.data.items[0])
+
     channelInfo = {
+      channelId: (response1.data.items[0].snippet.channelId === response3.data.items[0].id) ? response1.data.items[0].snippet.channelId : "channel id error",
       title: response3.data.items[0].snippet.title,
       description: (response3.data.items[0].snippet.description) ? response3.data.items[0].snippet.description : response1.data.items[0].snippet.description,
       customUrl: response3.data.items[0].snippet.customUrl,
@@ -28,39 +33,39 @@ async function retrievePlaylistitems(playlistid) {
       thumbnails: response3.data.items[0].snippet.thumbnails,
       viewCount: response3.data.items[0].statistics.viewCount
     }
-    // console.log(channelInfo)
 
-    response.data.items.forEach(x => {
+    response.data.items.forEach(async x => {
+      let apiInfo = await axios.get(`${apiVideoInfo}&id=${x.contentDetails.videoId}`)
       data = {
-        position: x.snippet.position,
+        position: x.snippet.position + 1,
         videoid: x.contentDetails.videoId,
         publishedAt: x.snippet.publishedAt,
         title: x.snippet.title,
         description: x.snippet.description,
-        thumbnails: x.snippet.thumbnails
+        thumbnails: x.snippet.thumbnails,
+        duration: apiInfo.data.items[0].contentDetails.duration
       }
+      // console.log(data.position, data.title)
       playlistItems.push(data)
     });
 
-    // playlistItems.push(...response.data.items);
-    // while (response.data.nextPageToken && i <= response.data.pageInfo.totalResults) {
     while (response.data.nextPageToken) {
       let url = `${playlistUrl}&playlistId=${playlistid}&pageToken=${response.data.nextPageToken}`
       response = await axios.get(url);
-      response.data.items.forEach(x => {
+      response.data.items.forEach(async x => {
+        let apiInfo = await axios.get(`${apiVideoInfo}&id=${x.contentDetails.videoId}`)
         data = {
-          position: x.snippet.position,
+          position: x.snippet.position + 1,
           videoid: x.contentDetails.videoId,
           publishedAt: x.snippet.publishedAt,
           title: x.snippet.title,
           description: x.snippet.description,
-          thumbnails: x.snippet.thumbnails
+          thumbnails: x.snippet.thumbnails,
+          duration: apiInfo.data.items[0].contentDetails.duration
         }
+        // console.log(data.position, data.title)
         playlistItems.push(data)
       });
-
-      // playlistItems.push(...response.data.items);
-      // playlistItems.push(data);
     }
   } catch(err) {
     console.log(err);
@@ -77,85 +82,65 @@ async function retrievePlaylistitems(playlistid) {
       thumbnails: playlistAttribs.snippet.thumbnails,
       playlistItemCount: playlistAttribs.contentDetails.itemCount,
     },
-    items: playlistItems
+    playlistItems
   };
 }
-
-// async function channelExist(channel) {
-//   const sql = `SELECT * FROM youtube_channel_owners where channel_id = '${channel}'`
-//   const result = await channelExists('test', sql)
-//   console.log(result)
-//   if (result.length === 0) {
-//     return false
-//   } else {
-//     return result
-//   }
-// }
-
-
-
 
 async function populateDatabase(dbname, data) {
   let sql = "INSERT INTO youtube_downloads (channel_owner_id, play_length, sequence, title, description, status, url)"
 }
 
-
-// dbCheck('test')
-//   .then(x => console.log(x));
-
-// retrievePlaylistitems(process.argv[2])
-//   .then(x => {
-//     console.log(Object.keys(x.items[0]));
-//     // console.log(`[${x.position}] ${x.title}`)
-//     // x.items.forEach(item => {
-//     //   console.log(`[${item.position+1}] ${item.title}`);
-//     // })
-//   });
-//   .then(x => {
-//     // console.log(x.kind, x.etag, x.id, "\n", Object.keys(x.snippet), "\n", Object.keys(contentDetails), Object.keys(status));
-//     // console.log(x.kind, x.etag, x.id);
-//     // console.log(Object.keys(x))
-//     x.forEach(item => {
-//       let version1 = `kind: ${item.kind} etag: ${item.etag} id: ${item.id}`
-//       let version2 = `
-//         [${item.snippet.position}]: ${item.snippet.title}
-//         ${item.snippet.publishedAt}
-//         ${item.snippet.description}
-//         ${item.snippet.thumbnails}
-//         ${item.snippet.playlistId}
-//         ${item.snippet.videoOwnerChannelId}, ${item.snippet.videoOwnerChannelTitle}
-//       `
-//       let version2a = `[${item.snippet.position}]: ${item.snippet.title} ${item.snippet.publishedAt}`
-
-//       let version3 = `
-//         ${item.contentDetails.videoId} ${item.contentDetails.videoPublishedAt} status: ${item.status.privacyStatus}
-//       `
-//       console.log(`${version2a}`);
-      
-//     });
-//   });
-
 async function main(id) {
-  let channelInfo, videoChannelId;
+  let addChannelResponse, videoChannelId;
   const list = await retrievePlaylistitems(id);
-  let channelStatus = await channelExists('test', list.channelId)
-  console.log(Object.keys(list))
+  // console.log(list.channelInfo, list.playlistInfo)
+  console.log(list.playlistItems.length)
+  let channelStatus = await channelExists('test', list.playlistInfo.channelId)
   if (!channelStatus) {
     let newChannelData = [
-      list.channelName,
-      list.title,
-      list.description,
-      list.channelId,
-      list.thumbnails
+      list.playlistInfo.channelName,
+      `https://www.youtube.com/${list.channelInfo.customUrl}`,
+      list.channelInfo.channelId,
+      list.playlistInfo.description,
+      list.channelInfo.customUrl,
+      list.channelInfo.publishedAt,
+      list.channelInfo.viewCount,
+      JSON.stringify(list.playlistInfo.thumbnails)
     ]
-    channelInfo = await addChannel('test', list.channelId);
-    console.log(`Channel ID: ${list.channelId}\n${channelStatus}\naddChannel Message: ${channelInfo}`);
+
+    addChannelResponse = await addChannel('test', newChannelData);
+
+    if (addChannelResponse) {
+      let message = `${color.brightGreen}Channel Id${color.Reset} ${color.brightCyan}${list.channelInfo.channelId}${color.Reset} ${(!channelStatus) ? `${color.brightGreen}is not archived in the database. Added with objid${color.Reset} ${color.brightCyan}${addChannelResponse.rows.insertId}${color.Reset}` : `${color.brightRed}error${color.Reset}`}`
+      console.log(message);
+    } else {
+      console.log(addChannelResponse);
+    }
   } else {
     videoChannelId = channelStatus.objid
-    console.log(`Channel ID: ${list.channelId}\nDatabase ID: ${videoChannelId}\nPlaylist: ${id} has ${list.items.length} items.`);
+    console.log(`${color.brightGreen}Channel${color.Reset} ${color.brightBlue}${channelStatus.owner_name}${color.Reset} ${color.brightGreen}has Database ID${color.Reset} ${color.brightBlue}${videoChannelId}${color.Reset}, ${color.brightGreen}Channel ID:${color.Reset} ${list.channelInfo.channelId} ${color.brightGreen}for Playlist${color.Reset} ${color.brightBlue}${id}${color.Reset} ${color.brightGreen}which has${color.Reset} ${color.brightYellow}${list.playlistItems.length}${color.Reset} ${color.brightGreen}items.${color.Reset}`);
   }
   // newChannelResults = await addVideo('test', )
+  // console.log(list.playlistItems[0])
+  let batch = list.playlistItems.map(item => {
+    let newVideoData = [
+      videoChannelId,
+      "https://www.youtube.com/watch?v=" + item.videoid,
+      item.duration,
+      item.title,
+      item.description,
+      item.position,
+      item.publishedAt,
+      JSON.stringify(item.thumbnails)
+    ]
+    // url, play_length, caption, description, sequence, upload_date, thumbnail
+    return newVideoData;
+  });
 
+  // console.log(batch.length)
+  batch.sort((a, b) => (a[5] > b[5]) ? 1 : (a[5] < b[5]) ? -1 : 0)
+  batch.forEach((array, i) => console.log(`[${i}] ${array[0]} ${array[5]}`))
+  // console.log(batch);
 }
 
 main(process.argv[2]);
